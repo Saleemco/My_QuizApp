2a138d0b5f88ed387356ec901722c4929a8fdcae
Saleemco <saleemahmad2504.com>
2025-08-30 16:47:50 +0100

    db php u

--- includes/db.php
+++ includes/db.php
@@ -1,15 +1,13 @@
 
 <?php
 // Database configuration
-$servername = getenv("DB_HOST");
-$username   = getenv("DB_USER");
-$password   = getenv("DB_PASS");
-$dbname     = getenv("DB_NAME");
-$port       = getenv("DB_PORT");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "quiz_appdb";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
-
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if (!$conn) {
